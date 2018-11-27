8a6a8f31059b3948a9770c28150937a9523ad090
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2018-11-27 22:29:07 +0100

    page stylized, added sass functionality, fixed few bugs

--- dist/index.js
+++ dist/index.js
@@ -20,14 +20,17 @@ function download(data, filename, type) {
 }
 
 class List {
-  constructor({ title, defaultValue, target } = {}) {
+  constructor({ title, defaultValue, target, nr } = {}) {
+    this.target = target ? target : document.querySelector("body");
+    this.nr = nr === undefined ? 0 : nr;
+
     this.title = title ? title : "To do list";
     if (defaultValue === undefined) defaultValue = [];
 
     this.values = [];
     this.counter = 0;
 
-    this.draw(target ? target : document.querySelector("body"));
+    this.draw(this.target);
     let i = 0;
     do {
       this.addList(this.counter, defaultValue[i]);
@@ -35,7 +38,7 @@ class List {
     } while (i < defaultValue.length);
   }
 
-  save(type = "") {
+  save(type = "", overSave = false) {
     const activeValues = this.values.filter(el => el.active);
 
     let values = [];
@@ -49,15 +52,18 @@ class List {
           ? []
           : JSON.parse(localStorage.getItem("list"));
 
-      curList.push({
-        values,
-        createdDate: `${date.toDateString()}, ${date.toLocaleTimeString()}`
-      });
+      if (overSave) curList[this.nr].values = values;
+      else
+        curList.push({
+          values,
+          createdDate: `${date.toDateString()}, ${date.toLocaleTimeString()}`,
+          id: curList.length
+        });
 
       localStorage.setItem("list", JSON.stringify(curList));
     }
 
-    console.log(values);
+    // console.log(values);
   }
 
   eventInput(e, nr) {
@@ -80,20 +86,80 @@ class List {
     target.parentNode.remove();
   }
 
+  alert(text, storageNameNev) {
+    return new Promise((res, rej) => {
+      res(true);
+
+      // const shadow = document.createElement("div");
+      // shadow.className = "c-dialog--shadow";
+      // const dialog = document.createElement("div");
+      // dialog.className = "c-dialog--dialog";
+
+      // const title = document.createElement("p");
+      // title.className = "c-dialog--title";
+      // title.innerText = text;
+      // dialog.appendChild(title);
+
+      // const yes = document.createElement("button");
+      // yes.innerText = "Yes, I'm sure!";
+      // yes.onclick = () => {
+      //   shadow.remove();
+      //   res(true);
+      // };
+      // dialog.appendChild(yes);
+
+      // const nope = document.createElement("button");
+      // nope.innerText = "No, how this is happend!";
+      // nope.onclick = () => {
+      //   shadow.remove();
+      //   res(false);
+      // };
+      // dialog.appendChild(nope);
+
+      // shadow.appendChild(dialog);
+
+      // document.querySelector("body").appendChild(shadow);
+
+      // if (storageNameNev !== undefined)
+      //   localStorage.setItem(storageNameNev, true);
+    });
+  }
+
+  removeList() {
+    this.alert("Do you really want to delete this list?").then(e => {
+      if (!e) return false;
+      let todel = JSON.parse(localStorage.getItem("list"));
+      delete todel[this.nr];
+      let afterdel = [];
+
+      todel.forEach(el => {
+        afterdel.push({
+          values: el.values,
+          createdDate: el.createdDate,
+          id: afterdel.length
+        });
+      });
+
+      if (afterdel.length === 0) localStorage.removeItem("list");
+      else localStorage.setItem("list", JSON.stringify(afterdel));
+
+      // console.log(todel);
+      helloPage(this.target);
+    });
+  }
+
   draw(target) {
     target.innerHTML = "";
 
     const wrapper = document.createElement("div");
     wrapper.className = "c-to-do-list";
 
-    const title = document.createElement("a");
+    const title = document.createElement("p");
     title.className = "c-to-do-list--title";
     title.innerText = this.title;
 
     this.linesWrapper = document.createElement("div");
     this.linesWrapper.className = "c-to-do-list--all-lines-wrapper";
-    this.linesWrapper.appendChild(title);
-    wrapper.appendChild(this.linesWrapper);
 
     const endButtons = document.createElement("div");
     endButtons.className = "c-to-do-list--end-buttons";
@@ -101,20 +167,52 @@ class List {
     const saveBrowser = document.createElement("button");
     saveBrowser.className = "c-to-do-list--end-buttons--browser-button";
     saveBrowser.innerText = "Save to browser storage";
-    saveBrowser.addEventListener("click", () => this.save("browser"));
-    endButtons.appendChild(saveBrowser);
+    saveBrowser.addEventListener("click", () => {
+      if (this.nr !== null) {
+        this.alert("Do you want to oversave your list?").then(e => {
+          if (e) this.save("browser");
+        });
+      } else this.save("browser");
+    });
 
     const backToMainMenu = document.createElement("button");
     backToMainMenu.className = "c-to-do-list--end-buttons--back-button";
     backToMainMenu.innerText = "Back to main menu";
-    backToMainMenu.addEventListener("click", () => helloPage(target));
-    endButtons.appendChild(backToMainMenu);
+    backToMainMenu.addEventListener("click", () => {
+      helloPage(this.target);
+      // if (localStorage.getItem("dnakagain")) helloPage(this.target);
+      // else
+      //   this.alert(
+      //     "Do you want to save your list to browser storage?",
+      //     "dnakagain"
+      //   ).then(e => {
+      //     if (e) this.save("browser", true);
+      //     helloPage(this.target);
+      //   });
+    });
 
-    // const saveDownload = document.createElement('button');
-    // saveDownload.className = 'c-to-do-list--end-buttons--download-button';
-    // saveDownload.innerText = 'Save by download to your disk';
-    // saveDownload.addEventListener('click', () => this.save('download'));
+    const removeList = document.createElement("button");
+    removeList.className = "c-to-do-list--end-buttons--remove-button";
+    removeList.innerText = "Remove 'to do' list";
+    removeList.addEventListener("click", () => this.removeList());
+
+    const saveDownload = document.createElement("button");
+    saveDownload.className = "c-to-do-list--end-buttons--download-button";
+    saveDownload.innerText = "Save by download to your disk";
+    saveDownload.addEventListener("click", () => this.save("download"));
+
+    const addNew = document.createElement("button");
+    addNew.className = "c-to-do-list--line--add-new-button";
+    addNew.innerText = "Add new line";
+    addNew.addEventListener("click", () => this.addList());
+
+    wrapper.appendChild(title);
+    wrapper.appendChild(this.linesWrapper);
+    endButtons.appendChild(saveBrowser);
+    endButtons.appendChild(backToMainMenu);
+    endButtons.appendChild(removeList);
     // endButtons.appendChild(saveDownload);
+    endButtons.appendChild(addNew);
 
     wrapper.appendChild(endButtons);
 
@@ -125,25 +223,19 @@ class List {
     this.counter++;
 
     const lineWrapper = document.createElement("div");
-    lineWrapper.className = "c-to-do-list--line--wrapper";
+    lineWrapper.className = "c-to-do-list--all-lines-wrapper--line-wrapper";
 
     const input = document.createElement("input");
     input.type = "text";
-    input.className = "c-to-do-list--line--input";
+    input.className = "c-to-do-list--all-lines-wrapper--line-wrapper--input";
     input.value = defaultValue;
     input.addEventListener("input", ev => this.eventInput(ev, nr));
     this.eventInput(input, nr);
     lineWrapper.appendChild(input);
 
-    const addNew = document.createElement("button");
-    addNew.className = "c-to-do-list--line--add-new-button";
-    addNew.innerText = "+";
-    addNew.addEventListener("click", () => this.addList());
-    lineWrapper.appendChild(addNew);
-
     const removeLine = document.createElement("button");
-    removeLine.className = "c-to-do-list--line--remove-button";
-    removeLine.innerText = "-";
+    removeLine.className =
+      "c-to-do-list--all-lines-wrapper--line-wrapper--remove-button";
     removeLine.addEventListener("click", ev => this.removeLine(ev, nr));
     lineWrapper.appendChild(removeLine);
 
@@ -154,55 +246,62 @@ class List {
 function helloPage(target = document.querySelector("body")) {
   target.innerHTML = "";
 
+  const wrapper = document.createElement("div");
+  wrapper.className = "c-home";
+
   const newList = document.createElement("button");
   newList.onclick = () => new List({ target });
-  newList.innerText = 'New "to do" list';
-  target.appendChild(newList);
+  newList.innerText = "New 'to do' list";
+  newList.className = "c-home--new-button";
+  wrapper.appendChild(newList);
 
   if (localStorage.getItem("list") !== null) {
     const prevs = document.createElement("select");
     prevs.onchange = ev => {
       const active = Array.from(ev.target.options).filter(e => e.selected)[0];
-      console.log(active.dataset.list.split(","));
+      // console.log(active.dataset.list.split(","));
       new List({
         target,
-        defaultValue: active.dataset.list.split(",")
+        defaultValue: active.dataset.list.split(","),
+        nr: active.dataset.id
       });
     };
+
+    //
+    //
+    prevs.onclick = ev => {
+      ev.stopPropagation();
+      if (prevs.className.indexOf("active") === -1)
+        prevs.className += " active";
+      else prevs.className = "c-home--list";
+    };
+    document.querySelector("body").addEventListener("click", ev => {
+      prevs.className = "c-home--list";
+    });
+    //
+    //
     const titleprev = document.createElement("option");
-    titleprev.innerText = 'Choose your "to do" list';
+    titleprev.innerText = "Choose your 'to do' list";
+    prevs.className = "c-home--list";
     prevs.appendChild(titleprev);
 
     JSON.parse(localStorage.getItem("list")).forEach((el, nr) => {
       const option = document.createElement("option");
-      option.innerText = el.createdDate;
+      option.innerText = `${el.values.length} elements --- ${el.createdDate}`;
       option.value = nr;
       option.dataset.list = el.values;
+      option.dataset.id = el.id;
+      option.title = el.values;
       prevs.appendChild(option);
     });
 
-    target.appendChild(prevs);
+    wrapper.appendChild(prevs);
   }
+  target.appendChild(wrapper);
 }
 
 (() => {
   console.log("js file loaded");
 
   helloPage(document.querySelector("#root"));
-
-  // document.querySelector('#n').onclick = () => {
-  //   new List({ target: document.querySelector('#root') });
-  // }
-
-  // if (localStorage.getItem('list') !== null) {
-  //   document.querySelector('#l').onclick = () => {
-  //     new List({
-  //       target: document.querySelector('#root'),
-  //       defaultValue: localStorage.getItem('list').split(',')
-  //     });
-  //   }
-  // }
-  // else document.querySelector('#l').remove();
-
-  // const z = new List({ target: document.querySelector('#root') });
 })();
